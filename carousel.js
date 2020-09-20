6db979e5fe494b4ec8fd1e88b2350e3e89800120
yobe8 <64393019+yobe8@users.noreply.github.com>
2020-09-21 07:58:01 +0900

    finished carousel
    아 dots 넘 어려웠다고!!!

--- carousel.js
+++ carousel.js
@@ -1,15 +1,20 @@
 const passImagesCon = document.querySelector('.carousel__passImages-container'),
     rightBtn = document.querySelector('.carousel__passImages__rightButton'),
-    leftBtn = document.querySelector('.carousel__passImages__leftButton');
-
+    leftBtn = document.querySelector('.carousel__passImages__leftButton'),
+    clickDots = document.getElementById('clickDots__list').querySelectorAll('button');
+    
 const image = new Image();
 image.className = 'js-carousel__PassImages__img';
 passImagesCon.insertBefore(image, rightBtn);
 
 let imgNum = 1;
 
-function clickDots() {
-    console.log('good');
+function clickDot(event) {
+    let dotId = parseInt(event.srcElement.id);
+    if(dotId !== imgNum) {
+        imgNum = dotId;
+    }
+    loadImages();
 }
 
 function rightClick() {
@@ -36,6 +41,11 @@ function init() {
     loadImages();
     leftBtn.addEventListener('click', leftClick);
     rightBtn.addEventListener('click', rightClick);
+    clickDots[0].addEventListener('click', clickDot);
+    clickDots[1].addEventListener('click', clickDot);
+    clickDots[2].addEventListener('click', clickDot);
+    clickDots[3].addEventListener('click', clickDot);
+    clickDots[4].addEventListener('click', clickDot);
     
 };
 
