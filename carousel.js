43a8c90b4c95c4eeb34eaef4bd4418557e85bcfc
yobe8 <64393019+yobe8@users.noreply.github.com>
2020-09-20 22:38:24 +0900

    updated css

--- carousel.js
+++ carousel.js
@@ -1,17 +1,17 @@
-const passImagesCon = document.querySelector('.carousel__passImages-container'), 
-rightBtn = document.querySelector('.carousel__passImages__rightButton'),
-leftBtn = document.querySelector('.carousel__passImages__leftButton');
+const passImagesCon = document.querySelector('.carousel__passImages-container'),
+    rightBtn = document.querySelector('.carousel__passImages__rightButton'),
+    leftBtn = document.querySelector('.carousel__passImages__leftButton');
 //querySelector와 getElement~ 의 차이를 알아보자 나중에.
 
 const image = new Image();
 image.className = 'js-carousel__PassImages__img';
-passImagesCon.insertBefore(image,rightBtn);
+passImagesCon.insertBefore(image, rightBtn);
 
 let imgNum = 1;
 
 function rightClick() {
     ++imgNum;
-    if(imgNum === 6) {
+    if (imgNum === 6) {
         imgNum = 1;
     }
     loadImages();
@@ -19,7 +19,7 @@ function rightClick() {
 
 function leftClick() {
     --imgNum;
-    if(imgNum === 0) {
+    if (imgNum === 0) {
         imgNum = 5;
     }
     loadImages();
@@ -28,8 +28,6 @@ function leftClick() {
 function loadImages() {
     image.src = `./images/${imgNum}.jpg`;
 };
-
-
 
 function init() {
     loadImages();
