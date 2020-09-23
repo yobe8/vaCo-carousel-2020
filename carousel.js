bdab669028969d399b88d24005a2a0ed67d49328
yobe8 <64393019+yobe8@users.noreply.github.com>
2020-09-23 11:43:55 +0900

    added comment by Ken

--- carousel.js
+++ carousel.js
@@ -2,7 +2,8 @@ const passImagesCon = document.querySelector('.carousel__passImages-container'),
     rightBtn = document.querySelector('.carousel__passImages__rightButton'),
     leftBtn = document.querySelector('.carousel__passImages__leftButton'),
     clickDots = document.getElementById('clickDots__list').querySelectorAll('button');
-    
+
+// KEN: 아래 3줄의 코드는 자바스크립트로 굳이 할 필요가 없을거 같고, HTML로 작업하시는 것이 좋을것 같네요!
 const image = new Image();
 image.className = 'js-carousel__PassImages__img';
 passImagesCon.insertBefore(image, rightBtn);
@@ -31,7 +32,7 @@ function leftClick() {
         imgNum = 5;
     }
     loadImages();
-};
+}; // KEN: 함수 선언식은 세미콜론을 붙이지 않습니다!
 
 function loadImages() {
     image.src = `./images/${imgNum}.jpg`;
@@ -41,6 +42,7 @@ function init() {
     loadImages();
     leftBtn.addEventListener('click', leftClick);
     rightBtn.addEventListener('click', rightClick);
+    // KEN: 반복문을 사용해보면 어떨까요?
     clickDots[0].addEventListener('click', clickDot);
     clickDots[1].addEventListener('click', clickDot);
     clickDots[2].addEventListener('click', clickDot);
